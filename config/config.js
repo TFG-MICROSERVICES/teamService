d8478eaf6520346259319bd3d66b1ff998cd4cfa
David Serrano Domínguez <daviidserranoo07@gmail.com>
2025-03-16 23:17:30 +0100

    cambios

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ export default {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        dialect: 'postgres',
         dialectOptions: {
             bigNumberStrings: true,
         },
